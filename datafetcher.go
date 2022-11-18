d375efeaacccd68214782aacc86971612c012d54
Scott Opell <me@scottopell.com>
2022-11-18 12:30:42 -0500

    Refactor how auth-tokens are discovered and set

--- datafetcher.go
+++ datafetcher.go
@@ -3,11 +3,20 @@ package main
 import (
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/user"
 	"time"
+)
+
+const (
+	endpointStatus  = "/agent/status"
+	endpointVersion = "/agent/version"
 )
 
 type DataFetcher struct {
@@ -25,10 +34,59 @@ func NewDataFetcher(host string, port int) *DataFetcher {
 	// TODO shorten this, take into account the refresh cycle
 	c := http.Client{Timeout: time.Duration(1) * time.Second, Transport: tr}
 
-	return &DataFetcher{
+	df := DataFetcher{
 		host:   host,
 		port:   port,
 		client: &c,
+	}
+
+	df.setAuthTokenFromEnv()
+
+	return &df
+}
+
+func (df *DataFetcher) setAuthTokenFromEnv() {
+	// TODO allow specifying auth_token via cli/env-var
+
+	// Start with list of common locations
+	// TODO - would be cool to have some "autodiscovery" here based on the currently running agent
+	// https://github.com/mitchellh/go-ps
+	currUser, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	authTokenLoc1 := fmt.Sprintf("/Users/%s/go/src/github.com/DataDog/datadog-agent/bin/agent/dist/auth_token", currUser.Username)
+	authTokenLoc2 := fmt.Sprintf("/Users/%s/code/datadog-agent/bin/agent/dist/auth_token", currUser.Username)
+	locations := [...]string{
+		authTokenLoc1,
+		authTokenLoc2,
+		"/etc/datadog-agent",
+		"/opt/datadog-agent/etc/auth_token",
+	}
+
+	authToken := ""
+	for _, loc := range locations {
+		auth_token, err := os.ReadFile(loc)
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				continue
+			} else {
+				break
+			}
+		}
+		s := string(auth_token)
+		if df.testAuthToken(s) {
+			authToken = s
+			break
+		} else {
+			continue
+		}
+	}
+
+	if authToken == "" {
+		panic(errors.New("No valid auth_tokens found!"))
+	} else {
+		df.AuthToken = authToken
 	}
 }
 
@@ -44,11 +102,11 @@ func (df *DataFetcher) constructRequest(endpoint string) *http.Request {
 }
 
 func (df *DataFetcher) testAuthToken(token string) bool {
-	req := df.constructRequest(endpointVersion)
-	// Remove empty auth-token
-	req.Header.Del("Authorization")
-
-	// Add the one we want to test
+	uri := fmt.Sprintf("https://%s:%d/%s", df.host, df.port, endpointVersion)
+	req, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return false
+	}
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 
 	resp, err := df.client.Do(req)
--- main.go
+++ main.go
@@ -1,68 +1,11 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/fs"
-	"log"
-	"os"
-	"os/user"
-
 	"code.rocketnine.space/tslocum/cview"
 )
 
-const (
-	endpointStatus  = "/agent/status"
-	endpointVersion = "/agent/version"
-)
-
-func getAuthToken(tester func(string) bool) (string, error) {
-	// Start with list of common locations
-	// TODO - would be cool to have some "autodiscovery" here based on the currently running agent
-	// https://github.com/mitchellh/go-ps
-	currUser, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	authTokenLoc1 := fmt.Sprintf("/Users/%s/go/src/github.com/DataDog/datadog-agent/bin/agent/dist/auth_token", currUser.Username)
-	authTokenLoc2 := fmt.Sprintf("/Users/%s/code/datadog-agent/bin/agent/dist/auth_token", currUser.Username)
-	locations := [...]string{
-		authTokenLoc1,
-		authTokenLoc2,
-		"/etc/datadog-agent",
-		"/opt/datadog-agent/etc/auth_token",
-	}
-
-	for _, loc := range locations {
-		auth_token, err := os.ReadFile(loc)
-		if err != nil {
-			if errors.Is(err, fs.ErrNotExist) {
-				continue
-			} else {
-				return "", err
-			}
-		}
-		s := string(auth_token)
-		if tester(s) {
-			return s, nil
-		} else {
-			continue
-		}
-	}
-
-	return "", errors.New("No auth locations passed")
-}
-
 func main() {
 	df := NewDataFetcher("localhost", 5001)
-
-	// TODO allow specifying auth_token via cli/env-var
-	authToken, err := getAuthToken(df.testAuthToken)
-	if err != nil {
-		panic(err)
-	}
-
-	df.AuthToken = authToken
 
 	app := IStatusApp{
 		app: cview.NewApplication(),
